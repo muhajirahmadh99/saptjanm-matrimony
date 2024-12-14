838f35f00fad6ee3b5e77f87f007986d84c9f416
muhajirahmadh <muhajir@hasnatech.com>
2024-12-14 09:10:57 +0530

    added

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ import { BrowserRouter } from "react-router-dom";
 import "flowbite";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename="/saptjanm-matrimony">
       <Provider store={appStore}>
         <App />
       </Provider>
