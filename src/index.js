4b5240ed77a35cea6e07c6a604c088ff18c1aa83
muhajirahmadh <muhajir@hasnatech.com>
2024-12-17 17:29:27 +0530

    added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 
 import { LoadingProvider } from "./layouts/LoadingContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
